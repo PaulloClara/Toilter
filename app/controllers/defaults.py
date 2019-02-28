aaa93fcfbe7c03f2fa51d22baa22d2ab0fe065dc
Paulo Ricardo <paulloclara@gmail.com>
2019-02-28 17:18:44 -0300

    Valores de teste inseridos nos htmls e rota about criada

--- app/controllers/defaults.py
+++ app/controllers/defaults.py
@@ -5,14 +5,19 @@ from flask import flash, redirect, url_for, render_template
 @app.route('/')
 @app.route('/home')
 def index():
-  return 'Home'
+  return render_template('home/index.html')
+
+
+@app.route('/about')
+def about():
+  return render_template('about/index.html')
 
 
 @app.route('/login')
 def login():
-  return 'Login'
+  return render_template('login/index.html')
 
 
 @app.route('/logout')
 def logout():
-  return 'Logout'
+  return redirect(url_for('index'))
