dcaac7ef537eff00b802a95e623fcb8d6bd928d8
Paulo Ricardo <paulloclara@gmail.com>
2019-02-28 15:47:03 -0300

    Estrutura basica de rotas e paginas criada

--- app/controllers/defaults.py
+++ app/controllers/defaults.py
@@ -1,8 +1,18 @@
 from app import app
+from flask import flash, redirect, url_for, render_template
 
 
 @app.route('/')
 @app.route('/home')
 def index():
-  return 'Hello World'
+  return 'Home'
 
+
+@app.route('/login')
+def login():
+  return 'Login'
+
+
+@app.route('/logout')
+def logout():
+  return 'Logout'
