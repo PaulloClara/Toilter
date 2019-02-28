77a1bd818433e4c63b604ab95ffde798f110c973
Paulo Ricardo <paulloclara@gmail.com>
2019-02-28 15:33:13 -0300

    Inicializando estrutura da aplicação

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Toilter
 Arquivos do curso da Julia Rizza no YouTube
+
+
+"pip3 install -r dependencies.txt"
---
+++ config.py
@@ -0,0 +1,6 @@
+DEBUG=True
+
+SQLALCHEMY_TRACK_MODIFICATIONS=True
+SQLALCHEMY_DATABASE_URI='sqlite+pysqlite:///storage.sqlite'
+
+SECRET_KEY = 'colocar-uma-chave-bem-segura-aqui'
